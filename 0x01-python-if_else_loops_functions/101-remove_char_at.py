c58b980599f1d4d331db1cbf69f959a2bd86baa8
KingHawkins <kinghawkins61@gmail.com>
2022-11-30 00:53:24 -0800

    all

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     copied = str[:]
-    get_rid = copied.replace(copied[n], '')
+    if n < 0:
+        get_rid = copied.replace(copied[:-n], '')
+    else:
+        get_rid = copied.replace(copied[n], '')
+
     return get_rid
