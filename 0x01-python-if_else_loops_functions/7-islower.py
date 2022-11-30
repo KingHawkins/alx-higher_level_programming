e11f7e459681a22d321ad90e4fffb8b8276e4121
KingHawkins <kinghawkins61@gmail.com>
2022-11-30 05:06:52 -0800

    add

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,10 +3,10 @@ def islower(c):
     lower_list = [chr(i) for i in range(97, 123)]
     for i in lower_list:
         if c == '' or isinstance(c, int) is True:
-            value = ''
+            value = 1
             break
         if c == '"':
-            value = ''
+            value = 1
         elif c == i:
             value = True
             break
