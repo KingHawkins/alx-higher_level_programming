4eec3aec3f604f48af1faa6545be36621ec5bfad
KingHawkins <kinghawkins61@gmail.com>
2022-12-01 08:35:31 -0800

    add

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     list_hidden = dir(hidden_4)
     for i in range(0, len(list_hidden)):
-        if '__' in list_hidden[i]:
-            continue
-        else:
+        if '__' not in list_hidden[i]:
             print("{}".format(list_hidden[i]))
