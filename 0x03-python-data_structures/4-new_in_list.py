7b00407e72a130fe13cba25b6efeac0932389354
KingHawkins <kinghawkins61@gmail.com>
2022-12-03 00:38:14 -0800

    all

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copied = ''
-    value = ''
     if idx < 0 or idx > len(my_list):
         copied = my_list[:]
     else:
-        value = my_list[:]
-        copied = value
+        copied = my_list[:]
         copied[idx] = element
     return copied
