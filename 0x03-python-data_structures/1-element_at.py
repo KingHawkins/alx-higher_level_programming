3214157d2ac80a26f99a6f5225cbfe0fff95f72b
KingHawkins <kinghawkins61@gmail.com>
2022-12-03 03:47:17 -0800

    adding

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     value = ''
     if idx < 0 or idx > len(my_list):
-        value = None
+        value = "None"
     else:
         value = my_list[idx]
     return value
