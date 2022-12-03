e3dee50ba42286bb3bb2f9aa68c3a4a13ccd8074
KingHawkins <kinghawkins61@gmail.com>
2022-12-03 05:22:15 -0800

    push

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    get_rid = ''
-    last = ''
-    converted = ''
-    new = ['c', 'C']
-    for i in range(len(my_string)):
-        if my_string[i] in new:
-            get_rid = list(my_string)
-            del get_rid[i]
-            continue
-
-    last = ''.join(str(i) for i in get_rid)
+    last = ''.join(str(i) for i in my_string if i not in 'Cc')
     return last
