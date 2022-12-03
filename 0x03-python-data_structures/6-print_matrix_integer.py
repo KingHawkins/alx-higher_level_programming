6191edf4f0863d0a1971bb51ded5e12845deeeec
KingHawkins <kinghawkins61@gmail.com>
2022-12-03 00:47:22 -0800

    add

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for mat in matrix:
         if len(mat) > 0:
-            print("{} {} {}".format(*mat))
+            print("{:d} {:d} {:d}".format(*mat))
         else:
             print('')
