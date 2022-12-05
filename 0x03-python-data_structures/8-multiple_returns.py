09658ccd67e7f7c5823b20d940ea68ef5618f78a
KingHawkins <kinghawkins61@gmail.com>
2022-12-05 03:31:21 -0800

    add

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) <= 0:
-        new_tup = ((len(sentence), None))
-    else:
-        new_tup = ((len(sentence), sentence[0]))
-        return new_tup
+    if(len(sentence) > 0):
+        return((len(sentence), sentence[0]))
+    return((len(sentence), None))
