95f5bdb591a960b3ab2c837e79c126798548aca3
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-05 11:37:40 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for mat in matrix:
         if mat:
-            print("{:d} {:d} {:d}".format(*mat))
+            print("{:d} {:d} {:d} {:d}".format(*mat))
         else:
             print()
