6b6e81d6a924d8f19c35bd8b6aa2da2349f74244
KingHawkins <kinghawkins61@gmail.com>
2022-12-05 04:00:27 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    list_hidden = dir(hidden_4)
-    for i in range(0, len(list_hidden)):
-        if '__' not in list_hidden[i]:
-            print("{}".format(list_hidden[i]))
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
---
+++ 0x02-python-import_modules/compile.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import dis
+import 102-magic_calculation
+did.dis(102-magic_calculation)
