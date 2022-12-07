92f71a42a0f40d6b08e281ce7065c3a831bcfdf8
KingHawkins <kinghawkins61@gmail.com>
2022-12-07 12:36:52 -0800

    all

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     new = []
     for item in set_1:
-       if item not in set_2:
-          new.append(item)
+        if item not in set_2:
+            new.append(item)
     for item in set_2:
-       if item not in set_1:
-          new.append(item)
+        if item not in set_1:
+            new.append(item)
     return new
