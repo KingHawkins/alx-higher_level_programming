946ec1e44e4c024a4e4d2267f46ef038c91e57d7
KingHawkins <kinghawkins61@gmail.com>
2022-12-19 12:08:52 -0800

    all

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value:
-            print("{:d}".format(value), end='')
+        print("{:d}".format(value), end='')
         print()
         return True
     except Exception:
