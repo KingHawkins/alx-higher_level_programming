ca481e6aa4ab1de6e19eb015069df63e527c910c
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 00:53:57 -0800

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
 class Square:
+    """Represents a square"""
     def __init__(self, size=0):
+        """Initialize a new square
+        Args:
+            size(int): new size for square
+        """
         if size < 0:
-            raise Exception("size must be >= 0")
-        elif size >= 0:
-            self._size = size
-        else:
-            raise Exception("size must be an integer")
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self._size = size
 
 
     def area(self):
+        """Return the area of square"""
         return self._size * self._size
