5ffbd9329af81fdb2ea117480d1966ac1cdb2580
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 06:44:45 -0800

    Create 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Define class square"""
+class Square:
+    """Represents class square"""
+    def __init__(self, size=0):
+        """Initialize new square
+        Args:
+             size(int): size of new square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initialize new square
+        Args:
+             value(int): size of new square
+        """
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(value, int) or not isinstance(value, float):
+            raise TypeError("size must be a number")
+        self.__size = value
+
+
+    def area(self):
+        """Returns area based on size"""
+        return self.__size ** 2
