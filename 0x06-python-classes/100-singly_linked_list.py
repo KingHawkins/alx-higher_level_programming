799c33e899c3cb1bf90dcbfed3c9d01195909b11
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 05:56:15 -0800

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-class Node:
 
+"""Define class Node."""
+
+class Node:
+    """Represent class node."""
     def __init__(self, data, next_node=None):
+        """Initialize node data
+        Args:
+            data(int): data to be stored in linked list
+            next_node(None): initialize linked list
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Get data"""
         return self.__data
 
     @data.setter
@@ -17,6 +26,7 @@ class Node:
 
     @property
     def next_node(self):
+        """Get next node."""
         return self.__next_node
 
     @next_node.setter
@@ -26,19 +36,28 @@ class Node:
         self.__next_node = value
 
 
+"""Define class SinglyLinkedList."""
 class SinglyLinkedList:
+"""represent class singlyLinkedlist."""
     __head = None
 
     def __init__(self):
+        """
+            Represent object."""
         pass
 
     def __str__(self):
+        """
+        Initialize linked list
+        """
         newList = []
         while(self.__head):
             newList.append(self.__head.data)
             self.__head = self.__head.next_node
+"""Returns new list"""
         return("\n".join([str(x) for x in sorted(newList)]))
 
     def sorted_insert(self, value):
+"""Instantiates node object."""
         newNode = Node(value, self.__head)
         self.__head = newNode
