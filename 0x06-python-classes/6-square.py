89cc3e108ef5b810c414953e0fbbe8aee0df6174
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 03:48:58 -0800

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,51 +1,60 @@
 #!/usr/bin/python3
+
+"""Define a new class Square."""
 class Square:
+"""Represents class Square."""
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize new square
+        Args:
+            size(int): size of new square
+            position(int): tuple containing the points the square will be plotted
+        """
         if size < 0:
             raise Exception("size must be >= 0")
         elif position[0] < 0 or position[1] < 0:
-            raise Exception("position must be a tuple of 2 positive integers")
-        elif size >= 0 or position[0] >= 0 and position[1] >= 0:
-            self.size = size
-            self.position = (position[0], position[1])
-        else:
-            raise Exception("size must be integer")
+            raise ValueError("position must be a tuple of 2 positive integers")
+        elif not isinstance(size, int):
+            raise TypeError("siz must be integer")
+        self.size = size
+        self.position = (position[0], position[1])
 
-
+    @property
     def size(self):
-        return self.size
+        return self.__size
 
-
+    @property
     def position(self):
-        return self.position
+        return self.__position
 
-
+    @position.setter
     def position(self, value):
         if value[0] < 0 or value[1] < 0:
             raise Exception("position must be a tuple of 2 positive integers")
         else:
-            self.position = (value[0], value[1])
+            self.__position = (value[0], value[1])
 
-
+    @size.setter
     def size(self, value):
             if value < 0:
-               raise Exception("size must be >= 0")
-            elif value >= 0:
-               self.size = value
+               raise ValueError("size must be >= 0")
+            elif isinstance(value, int) and value >= 0:
+               self.__size = value
             else:
-                raise Exception("size must be an integer")
+                raise TypeError("size must be an integer")
 
 
     def area(self):
-        return self.size * self.size
+        """Returns new area"""
+        return self.__size ** 2
 
 
     def my_print(self):
-        if self.size == 0:
+        """Plots the square on a graph."""
+        if self.__size == 0:
            print()
         else:
-            for i in range(self.size):
-                if self.position[0] > 0:
-                    print(' '*self.position[0] + "#"*self.size)
+            for i in range(self.__size):
+                if self.__position[0] > 0:
+                    print(' '*self.__position[0] + "#"*self.__size)
                 else:
-                    print("#"*self.size)
+                    print("#"*self.__size)
