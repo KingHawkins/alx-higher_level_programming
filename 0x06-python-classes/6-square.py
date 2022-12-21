13e74f33bf873c40dbf7fc513605c8a2ce7d2fb2
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 04:47:49 -0800

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,9 +54,6 @@ class Square:
            return
         else:
             for i in range(self.__size):
-                if self.__position[0] > 0 and self.__position[1] == 0:
+                if self.__position[0] > 0:
                     print(' '*self.__position[0] + "#"*self.__size)
-                elif self.__position[1] > 0:
-                    print('')
-                 else:
-                    print("#"*self.__size)
+                print("#"*self.__size)
