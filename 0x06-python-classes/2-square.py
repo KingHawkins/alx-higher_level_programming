14776cc46b7d0dec9c71d72d06601c66ae0f9bc7
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 00:41:45 -0800

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
     def __init__(self, size=0):
+        """Initialize size
+        
+        Args:
+            size(int): new size
+        """
         if int(size) < 0:
             raise Exception("size must be >= 0")
         elif isinstance(size, int) and size >= 0:
