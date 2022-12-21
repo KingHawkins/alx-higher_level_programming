bd2b18c54f8702db3a23d7d2cb300600955f1eb0
Bob Hawkins <111838325+KingHawkins@users.noreply.github.com>
2022-12-21 04:38:22 -0800

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,57 +1,51 @@
 #!/usr/bin/python3
-#6-square.py
-"""Define class Square."""
+# 6-square.py
+"""Define a class Square."""
+
 
 class Square:
-    """Represent class Square."""
-    
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize new square.
+
+        """Initialize a new square.
         Args:
-            size(int): size of new square
-            position(int, int): tuple containing the points the square will be plotted
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
- 
 
-    @property
-    def position(self):
-        """Gets position."""
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if value[0] < 0 or value[1] < 0  or
-            not isinstance(value, tuple) or
-            len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-
- 
     @property
     def size(self):
-        """Gets new size."""
-        return self.__size
- 
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if value < 0:
-             raise ValueError("size must be >= 0")
-        elif isinstance(value, int) and value >= 0:
-             self.__size = value
-        else:
-             raise TypeError("size must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
-        """Returns new area"""
-        return self.__size ** 2
-
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
         """Plots the square on a graph."""
