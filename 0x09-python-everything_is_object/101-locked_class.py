1a20a056518f2efee87f820fa9c47c6b0302fa06
KingHawkins <kinghawkins62@gmail.com>
2023-01-05 00:46:18 +0300

    add

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """defining a locked class"""
 class LockedClass:
-    """implementing the class object"""
+    """implementing the class object locked class"""
     __slots__ = ['first_name']
