a10b38bc7f7684ffcb65e773259d29dbe23ee667
KingHawkins <kinghawkins62@gmail.com>
2023-01-05 13:25:42 +0300

    adding

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import  numpy as np
 def matrix_mul(m_a, m_b):
     if (not all(isinstance(item, list) for item in m_b)
             and not all(isinstance(item, list) for item in m_a)):
@@ -7,7 +8,8 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list of lists")
     elif not all(isinstance(item, list) for item in m_a):
         raise TypeError("m_a must be a list of lists")
-    elif all(len(item) == 0 for item in m_a) and all(len(item)==0 for item in m_b):
+    elif (all(len(item) == 0 for item in m_a)
+            and all(len(item)==0 for item in m_b)):
         raise ValueError("m_a and m_b can't be empty")
     elif all(len(item) == 0 for item in m_a):
         raise ValueError("m_a can't be empty")
@@ -19,12 +21,16 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list")
     elif not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    elif (not all(isinstance(val, (int, float)) for item in m_a for val in item)
-            and not all(isinstance(val, (int, float)) for item in m_b for val in item)):
+    elif (not all(isinstance(val, (int, float))
+        for item in m_a for val in item)
+            and not all(isinstance(val, (int, float))
+                for item in m_b for val in item)):
         raise ValueError("m_a and m_b should contain only integers or floats")
-    elif not all(isinstance(val, (int, float)) for item in m_a for val in item):
+    elif (not all(isinstance(val, (int, float))
+        for item in m_a for val in item)):
         raise ValueError("m_a should contain only integers or floats")
-    elif not all(isinstance(val, (int, float)) for item in m_b for val in item):
+    elif (not all(isinstance(val, (int, float))
+        for item in m_b for val in item)):
         raise ValueError("m_b should contain only integers or floats")
     elif (not all(len(m_a[0]) == len(val) for val in m_a)
             and  not all(len(m_b[0]) == len(val) for val in m_b)):
@@ -35,4 +41,4 @@ def matrix_mul(m_a, m_b):
         raise TypeError("each row of m_b must be of the same size")
     elif (not all(len(m_a[0]) == len(m_b[0]) for i in range(len([m_a,m_b])))):
         raise TypeError("m_a and m_b can't be multiplied")
-
+    return np.matmul(m_a, m_b)
