c2400f48e9d4604a636ea44286a1ee985536ac91
KingHawkins <kinghawkins62@gmail.com>
2023-01-06 23:57:01 +0300

    changing 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,8 +15,8 @@ def add_integer(a, b=98):
         TypeError: If a or b aren't integer and/or float numbers
     """
 
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, int) or not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, int) or not isinstance(b, float):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
