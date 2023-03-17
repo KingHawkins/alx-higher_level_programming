a00dca8a668d46a919616d7c91b84ad2b3d4509b
KingHawkins <Kinghawkins61@gmail.com>
2023-03-18 00:23:52 +0300

    adding number two

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """selects names that begin with capital N"""
+
 import sys
 import MySQLdb
 
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3], port=3306)
     conn = db.cursor()
-    conn.execute("SELECT * FROM states
+    conn.execute("SELECT * FROM states\
             WHERE name LIKE 'N%' ORDER BY id ASC;")
     data = conn.fetchall()
     for key in data:
