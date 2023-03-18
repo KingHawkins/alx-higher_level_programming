24e349c13562c1dd054978178bd512eee1fda10b
KingHawkins <Kinghawkins61@gmail.com>
2023-03-18 16:52:32 +0300

    adding

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,9 +10,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     conn = db.cursor()
-    conn.execute("""SELECT cities.name\
-                  FROM states, cities\
-                  WHERE states.name = %s;""", (sys.argv[4],))
+    conn.execute("""SELECT DISTINCT cities.name\
+                  FROM states\
+                  JOIN cities\
+                  ON states.name = %s;""", (sys.argv[4],))
     states = conn.fetchall()
     print(", ".join([state[0] for state in states]))
     db.close()
