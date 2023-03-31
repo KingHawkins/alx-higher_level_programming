4062e08e42b6737444a7c24cb711ca47649533ff
KingHawkins <Kinghawkins61@gmail.com>
2023-03-31 23:33:00 +0300

    pushing all

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     user = sys.argv[1]
     password = sys.argv[2]
     re = get('https://api.github.com/user',
-                       auth=auth.HTTPBasicAuth(user, password))
+             auth=auth.HTTPBasicAuth(user, password))
     print(re.json().get('id'))
